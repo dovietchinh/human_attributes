570bb2aaa41603dd76a7e483d2836ce049fe6e00
dovietchinh <dovietchinh1998@gmail.com>
2022-06-21 15:52:10 +0700

    update file csv  version07

--- tool/train.sh
+++ tool/train.sh
@@ -1,7 +1,7 @@
 # cp config/human_attribute_7/note.txt 
 cd ..
 python source/train.py \
---cfg config/version_006/train_config.yaml \
---data config/version_006/data_config.yaml 
+--cfg config/version_007/train_config.yaml \
+--data config/version_007/data_config.yaml 
 
 
